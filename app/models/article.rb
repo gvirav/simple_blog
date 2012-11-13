12945da43cc844e13b9717fcf80433a7aba15fea
DBC Apprentice <apprentice@devbootcamp.com>
2012-11-12 18:11:54 -0800

    completed this exercise

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-
+  validates_presence_of :title, :body
+  validates :body, :format => { :with => /^((?!kitten).)*$/ }
   attr_accessible :body, :title
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-
+  validates :body, :length => { :maximum => 250 }
+  validates :body, :format => { :with => /^((?!kitten).)*$/ }
+  validates_presence_of :article_id
+  validates_presence_of :body
   attr_accessible :body
 end
